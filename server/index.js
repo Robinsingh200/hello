c01e479701e890182434d066025077e1b7218b68
Robinofficial200@ <your-email@example.com>
2026-06-29 10:08:34 +0530

    second

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(cors());
 
 app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from backend!" });
+  res.json({ message: "Hello from backend bhai !" });
 });
 
 app.get("/api/health", (req, res) => {
